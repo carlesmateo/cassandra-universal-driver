c1d44c0ca8fc751e29c6bdd44e323de4c7705b13
Carles Mateo <carles.mateo@gmail.com>
2014-04-06 00:49:08 +0200

    More clean code

--- cud.py
+++ cud.py
@@ -31,100 +31,85 @@ print "Content-Type: text/html"
 print ""
 
 st_arguments = cgi.FieldStorage()
-#for i in arguments.keys():
+#for i in st_arguments.keys():
 # print i
 # print ":"
-# print arguments[i].value
+# print st_arguments[i].value
 # print "<br />"
-
 
 b_error = 0
 i_error_code = 0
 s_html_output = ''
+b_use_keyspace = 1
+b_use_user_and_password = 1
 
-try:
-    s_cql = st_arguments['cql']
-    s_keyspace = st_arguments['keyspace']
-    s_cluster = st_arguments['cluster']
-    s_user = st_arguments['user']
-    s_password = st_arguments['password']
-except Exception:
-    b_error = 1
-    i_error_code = 100 # No params passed
+#TODO: port
+
+def getParam(st_arguments, s_param):
+    if st_arguments.has_key(s_param):
+        return str(st_arguments.getvalue(s_param))
+
+    return ''
+
+def returnError(i_error_code, s_error_description, s_format):
     s_html_output = str(i_error_code)
-    s_html_output = s_html_output +'\nError parameters not send\n\n'
-    # Log into Apache's error log
+    s_html_output = s_html_output + '\n' + s_error_description + '\n\n'
     log.info(s_html_output)
-    # Return via Tcp
     print s_html_output
     sys.exit()
+
+try:
+    s_cql = getParam(st_arguments, 'cql')
+    s_cluster = getParam(st_arguments, 'cluster')
+    if s_cql == '' || s_cluster == '':
+        returnError(i_error_code, 'Error parameters not send. Call with params: cql and cluster')
+except Exception:
+    returnError(i_error_code, 'Error parameters not send. Call with params: cql and cluster')
+
+try:
+    s_keyspace = getParam(st_arguments, 'keyspace')
+    if s_keyspace == '':
+        b_use_keyspace = 0
+except Exception:
+    b_use_keyspace = 0
+
+try:
+    s_user = getParam(st_arguments, 'user')
+    s_password = getParam(st_arguments, 'password')
+    if s_user == '' || s_password == '':
+        b_use_user_and_password = 0
+except Exception:
+    b_use_user_and_password = 0
+    s_user = ''
+    s_password = ''
 
 try:
     cluster = Cluster([s_cluster])
     session = cluster.connect()
 except Exception:
-    b_error = 1
-    i_error_code = 200 # Cannot connect
-    s_html_output = str(i_error_code)
-    s_html_output = s_html_output + '\nCannot connect to cluster\n\n'
-    # Log into Apache's error log
-    log.info(s_html_output)
-    # Return via Tcp
-    print s_html_output
-    sys.exit()
+    returnError(200, 'Cannot connect to cluster '+s_cluster)
 
-rows = session.execute("SELECT keyspace_name FROM system.schema_keyspaces")
-if KEYSPACE in [row[0] for row in rows]:
-    log.info("dropping existing keyspace...")
-    session.execute("DROP KEYSPACE " + KEYSPACE)
+if (b_use_keyspace == 1):
+    #log.info("setting keyspace...")
+    try:
+        session.set_keyspace(s_keyspace)
+    except:
+        returnError(210, 'Keyspace ' + s_keyspace + ' does not exist'')
 
-log.info("creating keyspace...")
-session.execute("""
-    CREATE KEYSPACE %s
-    WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
-    """ % KEYSPACE)
+# Samples:
+# Create Keyspace test
+# http://127.0.0.1/cgi-bin/cud.py?cluster=127.0.0.1&user=test&password=test&keyspace=&cql=CREATE%20KEYSPACE%20test%20WITH%20REPLICATION%20=%20{%20%27class%27:%20%27SimpleStrategy%27,%20%27replication_factor%27:%20%271%27%20}
 
-log.info("setting keyspace...")
-session.set_keyspace(KEYSPACE)
 
-log.info("creating table...")
-session.execute("""
-    CREATE TABLE mytable (
-        thekey text,
-        col1 text,
-        col2 text,
-        PRIMARY KEY (thekey, col1)
-    )
-    """)
-
-query = SimpleStatement("""
-    INSERT INTO mytable (thekey, col1, col2)
-    VALUES (%(key)s, %(a)s, %(b)s)
-    """, consistency_level=ConsistencyLevel.ONE)
-
-prepared = session.prepare("""
-    INSERT INTO mytable (thekey, col1, col2)
-    VALUES (?, ?, ?)
-    """)
-
-for i in range(10):
-    log.info("inserting row %d" % i)
-    session.execute(query, dict(key="key%d" % i, a='a', b='b'))
-    session.execute(prepared.bind(("key%d" % i, 'b', 'b')))
-
-future = session.execute_async("SELECT * FROM mytable")
-log.info("key\tcol1\tcol2")
-log.info("---\t----\t----")
+o_results = session.execute(s_cql)
 
 try:
-    rows = future.result()
+    rows = o_results.result()
 except Exception:
-    log.exeception()
+    returnError(300, 'Error executing query')
 
 for row in rows:
     log.info('\t'.join(row))
-
-session.execute("DROP KEYSPACE " + KEYSPACE)
 
 #if __name__ == "__main__":
 #    main()
