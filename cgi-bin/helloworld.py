88114fba5183c9936a030bdccd5074e6eaf4f34c
Carles Mateo <carles.mateo@gmail.com>
2014-04-11 02:54:53 +0200

    added () to avoid warnings in some IDE

--- cgi-bin/helloworld.py
+++ cgi-bin/helloworld.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-__author__ = 'carles.mateo@gmail.com'
+__author__ = 'Carles Mateo'
 
 # enable debugging
 import cgitb
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
+print("Content-Type: text/plain;charset=utf-8")
+print()
 
-print "Hello World!"
+print("Hello World!")
 
 
